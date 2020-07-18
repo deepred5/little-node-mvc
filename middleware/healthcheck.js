0d278271aa38691f8aac718bb56a7f0bff4842be
deepred5 <948931744@qq.com>
2020-07-18 09:50:14 +0800

    update: path

--- middleware/healthcheck.js
+++ middleware/healthcheck.js
@@ -1,7 +1,7 @@
 var reg = /\/do_not_delete\/health_check(\/)?$/
 module.exports = () => {
   return async (context, next) => {
-    if (reg.test(context.url)) {
+    if (reg.test(context.path)) {
       context.status = 200
       context.set('Content-Type', 'text/html')
       context.body = 'health_check'
